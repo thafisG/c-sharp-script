f2fb42ba034f09c4ac09b0a76bd3a19229709e8b
Thais Guedes <thais.guedes@ufpe.br>
2024-09-21 19:48:20 -0300

    Create codigo.cs

---
+++ beecrowd/desafio2/codigo.cs
@@ -0,0 +1,11 @@
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int valor1 = int.Parse(Console.ReadLine());
+        int valor2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"X = {valor1 + valor2}");
+    }
+
+}
