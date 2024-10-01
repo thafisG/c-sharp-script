49efc3d4d5527fdd6519ecb7119d13404c6e2293
Thais Guedes <thais.guedes@ufpe.br>
2024-10-01 18:14:46 -0300

    Create codigo.cs

---
+++ beecrowd/busca_na_internet/codigo.cs
@@ -0,0 +1,15 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        
+        int numero_de_click = int.Parse(Console.ReadLine());
+        
+        int resultado_clicks = numero_de_click * 4;
+        
+        Console.WriteLine(resultado_clicks);
+
+    }
+
+}
