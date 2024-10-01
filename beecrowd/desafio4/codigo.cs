e0d6e40b3138ad68edaa0fc1a7e7a33bba0d3f30
Thais Guedes <thais.guedes@ufpe.br>
2024-10-01 14:10:07 -0300

    Create codigo.cs

---
+++ beecrowd/desafio4/codigo.cs
@@ -0,0 +1,15 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        int numero1 = int.Parse(Console.ReadLine());
+        int numero2 = int.Parse(Console.ReadLine());
+        
+        int resultado = numero1 + numero2;
+        
+        Console.WriteLine($"SOMA = {resultado}");
+
+    }
+
+}
