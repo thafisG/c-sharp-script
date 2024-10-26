956a9a4bb97f4c1689a1c45d714f42544aa13ed0
Thais Guedes <thais.guedes@ufpe.br>
2024-10-26 19:38:21 -0300

    Create codigo.cs

---
+++ beecrowd/desafio5/codigo.cs
@@ -0,0 +1,12 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        int numero1 = int.Parse(Console.ReadLine());
+        int numero2 = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine($"PROD = {numero1 * numero2}")
+    }
+
+}
